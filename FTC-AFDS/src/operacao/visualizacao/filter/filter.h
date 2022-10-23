7f16ce72d31db6160825cae96e54b272912112c6
Matheus Fernandes <80917379+wolfMatheus@users.noreply.github.com>
2022-10-23 20:48:57 -0300

    Delete FTC-AFDS directory

--- FTC-AFDS/src/operacao/visualizacao/filter/filter.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "../../dot/dot.h"
-
-/**
- * Filtra as transicoes presentes no DOTFormat informado, de forma que transicoes que partem e alcancam estados correspondente
- * sejam representadas por somente um arco no grafo do arquivo DOT.
- * @param dotFormat: Informacoes DOT alvo.
-*/
-void filterTransitions(DOTFormat *dotFormat);
--- FTC-AFDS/src/parametros/parametros.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#define PARAMS_H
-#define DOT "--dot"
-#define OUTPUT "--output"
-#define COMPLEMENTO "--complemento"
-#define INTERSECAO "--intersecao"
-#define UNIAO "--uniao"
-#define MINIMIZACAO "--minimizacao"
-#define RECONHECER "--reconhecer"
